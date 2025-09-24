cfd9e8538dff000dd19d5c719914159904e5db7c
sheepingshroom <gustavidunsloth@hotmail.com>
2025-09-24 20:08:12 +0200

    extensions folder

--- puppet/config.py
+++ puppet/config.py
@@ -5,7 +5,7 @@ HEADLESS = False
 ROOT = Path(__file__).parents[1]
 SESSION_DIR = ROOT / "data" / "session"
 LOG_DIR = ROOT / "data" / "logs"
-UBLOCK_PATH = ROOT / "puppet" / "extensions" / "ublock"
+UBLOCK_PATH = ROOT / "puppet" / "extensions"
 
 if not SESSION_DIR.exists():
     os.mkdir(SESSION_DIR)
