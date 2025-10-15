796338c18cb85708eb2c682596f395ddd322d2fc
sheepingshroom <gustavidunsloth@hotmail.com>
2025-10-15 14:51:18 +0200

    add puppet dir

--- src/config.py
+++ src/config.py
@@ -27,7 +27,7 @@ SLANTS_TRAIN = SLANT_DIR / "slants_train.pkl"
 MODELS_DIR = ROOT / "data" / "models"
 
 if not PUPPETS_DIR.exists():
-    os.mkdir(SESSION_DIR)
+    os.mkdir(PUPPETS_DIR)
 
 if not MODELS_DIR.exists():
     os.mkdir(SESSION_DIR)
